f7841080a920b9fa4ef202dbc2d21adb4892caad
Leo <leonardo_cavalcante@msn.com>
2018-01-03 16:02:12 -0200

    notification count

--- webview.js
+++ webview.js
@@ -2,9 +2,9 @@
 
 module.exports = Franz => {
   const getMessages = function getMessages() {
-    const notifications = document.querySelectorAll('.pp-notifications');
-
-    Franz.setBadge(0, notifications.length >= 1 ? 1 : 0);
+    const notif = document.getElementById("notifications_button");
+    let hasNotification = notif.classList.contains('pp-active')
+    Franz.setBadge(0, hasNotification ? parseInt( notif.innerHTML ) : 0);
   };
 
   Franz.loop(getMessages);
