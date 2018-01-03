39adf748cb7323dc5e5f8480346ed9b9e1077d9b
Leo <leonardo_cavalcante@msn.com>
2018-01-03 16:13:49 -0200

    readme

--- webview.js
+++ webview.js
@@ -3,8 +3,8 @@
 module.exports = Franz => {
   const getMessages = function getMessages() {
     const notif = document.getElementById("notifications_button");
-    let hasNotification = notif.classList.contains('pp-active')
-    Franz.setBadge(0, hasNotification ? parseInt( notif.innerHTML ) : 0);
+    let hasNotification = notif.classList.contains('pp-active');
+    Franz.setBadge(0, hasNotification ? parseInt( notif.innerHTML ) : 0 );
   };
 
   Franz.loop(getMessages);
